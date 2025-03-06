1c1a8fa19787516289eb5c995947f25eba362094
Dan Castillo <dan@21ninjas.es>
2025-03-06 12:07:50 +0000

    Fix

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -3,6 +3,6 @@
 Rails.application.config.after_initialize do
   if defined?(Rails::Server)
     # Schedule the initial check and reschedule itself
-    CheckDomainStatusJob.set(wait: 5.minutes).perform_later
+    CheckDomainStatusJob.set(wait: 1.minute).perform_later
   end
 end
