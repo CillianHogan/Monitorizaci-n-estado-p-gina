c7609f21c4f1a6f1659e1f66bcbdd5928c513105
Dan Castillo <dan@21ninjas.es>
2025-03-06 10:53:03 +0000

    Config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern, &:html
+
+  private
+
+  def after_sign_in_path_for(resource)
+    domains_path
+  end
+
 end
