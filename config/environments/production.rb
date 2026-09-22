067ad8d34356b488f0615b99b0f05067941d46fe
Cillian Aithami Hogan Lobato <hoganlobato@gmail.com>
2026-09-22 12:24:47 +0000

    configurar conexion queue apuntando a database url en produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,3 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
-SolidQueue.connects_to = { database: { writing: :primary } }
